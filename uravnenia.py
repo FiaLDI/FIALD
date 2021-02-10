23e7d920510296b8e2b25e0fdbdb86d9916297ad
Fiald <leon.youtube2017@yandex.ru>
2021-02-10 21:30:05 +0300

    engl

--- uravnenia.py
+++ uravnenia.py
@@ -1,36 +1,50 @@
 from math import sqrt
-sluchai={"1":"Линейные уравнения вида ax+b=0",
-         "2":"Квадратные уравнения вида ax**2+bx+c= 0",
-         "3":"Логарифмические уравнения решаемые по определению"}
 
-def linaxb(a,b):
-    x=-b/a
-    print("Решение: ", x)
 
-def kvadrat(a,b,c):
-    D=b**2 - 4 * a * c
-    print("Дискриминант:", D)
-    x1=-b+sqrt(D)/2*a
-    x2=-b-sqrt(D)/2*a
-    print("Корень уравнения 1 -", x1)
-    print("Корень уравнения 1 -", x2)
+#Function dictionary
+sluchai = {"1": "Linear equations of the form  ax+b=0",
+           "2": "Quadratic equations of the form  ax**2+bx+c= 0",
+           "3": "Logarithmic equations solved by definition "}
 
+
+#Solving Equations (1)
+def linaxb(a, b):
+    x = -b / a
+    print("Resolve: ", x)
+
+
+#Solving Equations (2)
+def kvadrat(a, b, c):
+    D = b ** 2 - 4 * a * c
+    print("Discriminant :", D)
+    x1 = -b + sqrt(D) / 2 * a
+    x2 = -b - sqrt(D) / 2 * a
+    print("Root of the equation 1 -", x1)
+    print("Root of the equation 2 -", x2)
+
+
+#Solving Equations (3)
 def logar(a, b):
-    x=a**b
-    if x>0 and a>1 and a != 1:
-        print("Корень уравнения -", x)
+    x = a ** b
+    if x > 0 and a > 1 and a != 1:
+        print("Root of the equation -", x)
     else:
-        print("Нельзя")
+        print("It is impossible ")
 
-znachenie=int(input("Введите операцию: "))
-if znachenie==1:
-    print(sluchai.get("1"))
-    linaxb(a=int(input("Введите аргумент a: ")), b=int(input("Введите аргумент b: ")))
-if znachenie==2:
-    print(sluchai.get("2"))
-    kvadrat(a=int(input("Введите аргумент a: ")), b=int(input("Введите аргумент b: ")), c=int(input("Введите аргумент c: ")))
-if znachenie==3:
-    print(sluchai.get("3"))
-    logar(a=int(input("Введите аргумент a: ")), b=int(input("Введите аргумент b: ")))
-
-
+#Loop Solving Equation
+run=True
+while run:
+    znachenie = int(input("Enter operation: "))
+    if znachenie == 1:
+        print(sluchai.get("1"))
+        linaxb(a=int(input("Enter the argument a: ")), b=int(input("Enter the argument b: ")))
+    if znachenie == 2:
+        print(sluchai.get("2"))
+        kvadrat(a=int(input("Enter the argument a: ")), b=int(input("Enter the argument b: ")),
+                c=int(input("Enter the argument c: ")))
+    if znachenie == 3:
+        print(sluchai.get("3"))
+        logar(a=int(input("Enter the argument  a: ")), b=int(input("Enter the argument b: ")))
+    if znachenie == 0:
+        print("Exit.")
+        run=False
--- uravneniyatreg.py
+++ uravneniyatreg.py
@@ -1,61 +1,72 @@
+# Sinus Dictionary
 sins = {"0": "0",
-         "1/2":"pi/6",
-         "sqrt(2)/2":"pi/4",
-         "sqrt(3)/2":"pi/3",
-         "1":"pi/2",
-         "3pi/2": "-1"
-         }
+        "1/2": "pi/6",
+        "sqrt(2)/2": "pi/4",
+        "sqrt(3)/2": "pi/3",
+        "1": "pi/2",
+        "3pi/2": "-1"
+        }
+
+
+# Cossinus Dictionary
 coss = {"1": "0",
-            "sqrt(3)/2":"pi/6",
-            "sqrt(2)/2":"pi/4",
-            "1/2":"pi/3",
-            "0":"pi/2",
-            "-1":"pi",
-            }
+        "sqrt(3)/2": "pi/6",
+        "sqrt(2)/2": "pi/4",
+        "1/2": "pi/3",
+        "0": "pi/2",
+        "-1": "pi",
+        }
+
+
+# tangens Dictionary
 tgg = {"0": "0",
-           "sqrt(3)/3":"pi/6",
-           "1":"pi/4",
-           "sqrt(3)":"pi/3",
-           }
-print("Решает уравнения вида cosx=b")
-print("1 - sinx, 2 - cosx, 3 - tgx 0 - выход")
+       "sqrt(3)/3": "pi/6",
+       "1": "pi/4",
+       "sqrt(3)": "pi/3",
+       }
 
 
+print("Solves equations of the form  cosx=b")
+print("1 - sinx, 2 - cosx, 3 - tgx 0 - exit")
+
+
+# The function that solves the equation (1)
 def uravnsin(b):
-    sinx=b
+    sinx = b
     x = sins.get(sinx)
     print(x)
 
+
+# The function that solves the equation (2)
 def uravncos(b):
-    cosx=b
+    cosx = b
     x = coss.get(cosx)
     print(x)
 
+
+# The function that solves the equation (3)
 def uravntg(b):
-    tgx=b
+    tgx = b
     x = coss.get(tgx)
     print(x)
 
-run=True
-while run:
+
+
+#Loop Solving Equation
+runing = True
+while runing:
     try:
-        operacia=int(input("Введите операцию: "))
-        if operacia==1:
-            b = input("Введите b: ")
+        operation = int(input("Enter operation: "))
+        if operation == 1:
+            b = input("Enter b: ")
             uravnsin(b)
-        if operacia==2:
-            r=input("Введите b: ")
+        if operation == 2:
+            r = input("Enter b: ")
             uravncos(r)
-        if operacia==3:
-            d=input("Введите b: ")
+        if operation == 3:
+            d = input("Enter b: ")
             uravntg(d)
-        if operacia==0:
-            run=False
+        if operation == 0:
+            runing = False
     except:
-        print("Ошибка.")
-
-
-
-
-
-
+        print("Error.")
